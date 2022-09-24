52f765f866ca78ea31996bf29eeb616de2ec03e8
Saro Henyo <cryptohenyo@gmail.com>
2022-09-24 14:17:54 +0800

    initial commit

---
+++ src/main/java/saro/fullstacktodolist/FullstackTodolistApplication.java
@@ -0,0 +1,13 @@
+package saro.fullstacktodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackTodolistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/saro/fullstacktodolist/FullstackTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package saro.fullstacktodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
